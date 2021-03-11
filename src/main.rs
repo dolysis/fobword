982abc988ea5fccd3cbc167b3b32c08c46c695aa
iRaiko <jlardenoije@gmail.com>
2021-03-11 14:11:13 +0100

    temporary stuff in main

--- src/main.rs
+++ src/main.rs
@@ -3,9 +3,26 @@
 // the MPL was not distributed with this file, You
 // can obtain one at http://mozilla.org/MPL/2.0/.
 
-mod writer;
+use std::fs::OpenOptions;
+use std::io::{stdin, stdout, Write};
+use rusqlite::Connection;
 
-fn main() 
+mod writer;
+mod error;
+
+fn main()
 {
-    
+    let conn = Connection::open("./password.db3");
+    println!("{}", conn.is_ok());
+    let mut file = match OpenOptions::new().write(true).open(r"/dev/hidg0")
+    {
+        Ok(f) => f,
+        Err(e) => { println!("File open failed: {}\n Try opening with sudo.", e); panic!() } ,
+    };
+    writer::write_password("abcdefASDAS0[SADUIA{Sd][][;ad", &mut file).expect("works");
 }
+
+// Create macro
+// Use macro
+// Change Macro
+// Delete Macro?
