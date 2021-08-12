9893c1cb274f0cf8e2c24aeb08edd10cb72c6ec1
iRaiko <jlardenoije@gmail.com>
2021-08-12 10:19:57 +0200

    Update main.rs

--- src/main.rs
+++ src/main.rs
@@ -38,6 +38,6 @@ fn default_config() -> Result<Config<AppSettings>, DataHandleError>
 {
     let settings = AppSettings { input: "/dev/hidraw0".to_owned(), output: "/dev/hidg0".to_owned(), macro_key: vec![0x01, 0, 0x38u8, 0, 0, 0, 0, 0,]};
     let mut data = Data::new();
-    data.insert("ff14".to_owned(), "Squ3r33n1xP4ssW0rd".to_owned(), None, None)?;
+    data.insert("test".to_owned(), "testpassword".to_owned(), None, None)?;
     Ok(Config::new(Some(settings), Some(data)))
 }
